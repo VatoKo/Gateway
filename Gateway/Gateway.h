5aaae88e476d2702e68da702517e18b00cdafddb
VatoKo <vkost16@freeuni.edu.ge>
2020-07-05 21:18:31 +0400

    Initial commit

---
+++ Gateway/Gateway.h
@@ -0,0 +1,19 @@
+//
+//  Gateway.h
+//  Gateway
+//
+//  Created by Vato Kostava on 7/5/20.
+//  Copyright © 2020 Vato Kostava. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Gateway.
+FOUNDATION_EXPORT double GatewayVersionNumber;
+
+//! Project version string for Gateway.
+FOUNDATION_EXPORT const unsigned char GatewayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gateway/PublicHeader.h>
+
+
---
+++ Gateway/Gateway.swift
@@ -0,0 +1,17 @@
+//
+//  Gateway.swift
+//  Gateway
+//
+//  Created by Vato Kostava on 7/5/20.
+//  Copyright © 2020 Vato Kostava. All rights reserved.
+//
+
+import Foundation
+
+public struct Request {
+    
+    public static func request() {
+        print("Request created...")
+    }
+    
+}
